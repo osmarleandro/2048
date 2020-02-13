a52cd40d7dea8da7df362a7b0ff68cd3e0df7985
Osmar Leandro <osmarleandrods@gmail.com>
2020-02-13 00:14:40 -0300

    Create and encapsulate field SuperClassExtracted#gameCounter

--- src/main/java/put/ci/cevo/games/board/SuperClassExtracted.java
+++ src/main/java/put/ci/cevo/games/board/SuperClassExtracted.java
@@ -1,4 +1,13 @@
 package put.ci.cevo.games.board;
 
 public class SuperClassExtracted {
+    private int gameCounter;
+
+    public int getGameCounter() {
+        return gameCounter;
+    }
+
+    public void setGameCounter(int gameCounter) {
+        this.gameCounter = gameCounter;
+    }
 }
