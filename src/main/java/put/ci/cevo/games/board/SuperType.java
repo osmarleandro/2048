1ad73efa5fdd91ed0ba3610ddf5053963fcff678
Osmar Leandro <osmarleandrods@gmail.com>
2020-02-13 00:13:51 -0300

    ref: Extract supertype at Board with params: select all members and name SuperType

---
+++ src/main/java/put/ci/cevo/games/board/SuperType.java
@@ -0,0 +1,29 @@
+package put.ci.cevo.games.board;
+
+public abstract class SuperType extends SuperClassExtracted {
+    public abstract int getWidth();
+
+    public abstract int getHeight();
+
+    /**
+     * @param row
+     *            0-based index
+     * @param col
+     *            0-based index
+     */
+    public abstract int getValue(int row, int col);
+
+    public abstract void setValue(int row, int col, int color);
+
+    /**
+     * @param pos
+     *            pos is not 0-based. It is currently implementation dependent
+     */
+    public abstract int getValue(int pos);
+
+    public abstract SuperClassExtracted clone();
+
+    public BoardPos newBoard(int row, int column) {
+        return new BoardPos(row, column);
+    }
+}
